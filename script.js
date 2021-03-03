2398ad99a163a9be80573ccc0a31a6b899f4b0d6
Shadesturkenboom <79255028+Shadesturkenboom@users.noreply.github.com>
2021-03-03 11:55:51 +0100

    Veranderingen 2
    Deze veranderingen heb ik gemaakt 2

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 
+Chart.defaults.global.defaultFontColor = '#ffffff'
+
 var brandstof = document.getElementById('line').getContext('2d');
 
 var myLineChart = new Chart(brandstof, {
     type: 'line',
      data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        labels: ['Dag 1', 'Dag 4', 'Dag 8 ', 'Dag 12', 'Dag 16', 'Dag 20', 'Dag 24'],
         datasets: [{
-            label: 'My First dataset',
-            backgroundColor: 'rgb(255, 99, 132)',
+            label: 'Brandstof',
+            backgroundColor: 'black',
             borderColor: 'rgb(255, 99, 132)',
-            data: [0, 10, 5, 2, 20, 30, 45]
+            data: [86000000, 71700000, 57400000, 43100000, 28800000, 14500000, 200000]
         }]
     },
 
@@ -23,15 +25,15 @@ var eten = document.getElementById('bar').getContext('2d');
 var myBarChart = new Chart(eten, {
     type: 'bar',
  data: {
- 	labels:['vega', 'vlees', 'vis', 'vogel'],
+ 	labels:['vegetarisch', 'vlees', 'veganistisch', 'vis'],
     datasets: [{
+        label: 'none',
         barPercentage: 0.5,
         barThickness: 6,
         maxBarThickness: 8,
         minBarLength: 2,
-        data: [20, 30, 40, 50, 0],
-        backgroundColor: 'red',
-        borderColor: 'red',
+        data: [718 , 1010, 318, 942, 0],
+        backgroundColor: ['#FFA500', '#FF4040', '#7171C6', '#00BFFF'],
         borderWidth: '10'
     }]
 },
@@ -44,10 +46,10 @@ var aantallen = document.getElementById('donut').getContext('2d');
 var myDoughnutChart = new Chart(aantallen, {
     type: 'doughnut',
     data: {
-    	labels: [ 'Red','Yellow','Blue' ],
+    	labels: [ 'Leeftijd 0-15','Leeftijd 15-30','Leeftijd 30-45', 'Leeftijd 45-60','Leeftijd 65+'   ],
 	    datasets: [{
-	        data: [10, 20, 30],
-	        backgroundColor: ['red', 'green', 'yellow']
+	        data: [2, 6, 42, 32, 18],
+	        backgroundColor: ['#FF83FA', '#388E8E', '#00FFFF', '#FF6347', '#AB82FF']
 	   
 	    }],	 
 },
